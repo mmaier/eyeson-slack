f3ee4038f2e0eb76c76447dc6c36433a57001691
Michael Maier <michael.maier@visocon.com>
2016-10-20 09:57:18 +0200

    adds delayed job config

---
+++ config/initializers/delayed_jobs.rb
@@ -0,0 +1,5 @@
+Delayed::Worker.destroy_failed_jobs = false
+Delayed::Worker.sleep_delay = 5
+Delayed::Worker.max_attempts = 2
+Delayed::Worker.max_run_time = 10.seconds
+Delayed::Worker.read_ahead = 10
