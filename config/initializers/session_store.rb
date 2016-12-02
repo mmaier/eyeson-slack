30089e8d876b35de64bd207b36526a7d9a2b0f0e
Michael Maier <michael.maier@visocon.com>
2016-12-02 15:01:10 +0100

    rename session key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_slack_session', expire_after: 10.years
+Rails.application.config.session_store :cookie_store, key: '_simsa_session', expire_after: 10.years
