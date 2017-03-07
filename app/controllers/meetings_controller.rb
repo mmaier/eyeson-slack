2c474515a1bde2625d83549e1df81a96cfd0455a
Michael Maier <michael.maier@visocon.com>
2017-03-07 16:03:01 +0100

    remove account confirm

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -5,7 +5,7 @@ class MeetingsController < ApplicationController
   rescue_from SlackApi::RequestFailed, with: :enter_room
 
   before_action :authorized!
-  before_action :user_confirmed!
+  #before_action :user_confirmed!
   before_action :channel_exists!
   before_action :user_belongs_to_team!
   before_action :scope_required!
