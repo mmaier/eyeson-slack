838f40b97256748b2d1984de380a6f00320adf81
Michael Maier <michael.maier@visocon.com>
2017-06-14 11:05:19 +0200

    fixes cool renderer secrets

--- config/initializers/cool_renderer.rb
+++ config/initializers/cool_renderer.rb
@@ -4,6 +4,6 @@ require Rails.root.join 'lib', 'cool_renderer'
 
 module CoolRenderer
   IMG_SERVICE = Rails.configuration.services['image_service']
-  IMG_USER    = Rails.application.secrets[:img_service][:user]
-  IMG_PASSWD  = Rails.application.secrets[:img_service][:password]
+  IMG_USER    = Rails.application.secrets[:img_service]['user']
+  IMG_PASSWD  = Rails.application.secrets[:img_service]['password']
 end
