2e68c7a85330de2b90967272a46167e77d78b03d
Michael Maier <michael.maier@visocon.com>
2017-06-19 15:15:02 +0200

    clear question layer

--- app/jobs/questions_display_job.rb
+++ app/jobs/questions_display_job.rb
@@ -7,24 +7,43 @@ class QuestionsDisplayJob < ApplicationJob
     username = args[1]
     question = args[2]
 
-    if channel.last_question_at && channel.last_question_at > 10.seconds.ago
-      QuestionsDisplayJob.perform_later(channel.id,
-                                        username,
-                                        question)
-    else
+    if question && question_active?(channel)
+      requeue(channel, username, question)
+    elsif question
       set_layer(channel, username, question)
+    elsif clearable?(channel)
+      clear_layer(channel)
     end
   end
 
   private
 
-  def requeue; end
+  def question_active?(channel)
+    channel.last_question_at && channel.last_question_at > 10.seconds.ago
+  end
+
+  def clearable?(channel)
+    channel.last_question_at && channel.last_question_at < 10.seconds.ago
+  end
+
+  def requeue(channel, username, question)
+    QuestionsDisplayJob.perform_later(channel.id,
+                                      username,
+                                      question)
+  end
 
   def set_layer(channel, username, question)
     return if channel.access_key.blank?
     layer = Eyeson::Layer.new(channel.access_key)
     layer.create(url: question_image(username, question))
     channel.update last_question_at: Time.current
+    QuestionsDisplayJob.set(wait: 10.seconds).perform_later(channel.id.to_s)
+  end
+
+  def clear_layer(channel)
+    return if channel.access_key.blank?
+    Eyeson::Layer.new(channel.access_key).destroy
+    channel.update last_question_at: nil
   end
 
   def question_image(username, question)
--- spec/jobs/questions_display_job_spec.rb
+++ spec/jobs/questions_display_job_spec.rb
@@ -9,20 +9,39 @@ RSpec.describe QuestionsDisplayJob, type: :active_job do
     create(:channel)
   end
 
-  it 'should perform with channel id, username and question' do
+  it 'should perform set_layer with channel id, username and question' do
     job.expects(:set_layer).with(channel, 'user', 'Question')
     job.perform(channel.id, 'user', 'Question')
   end
 
-  it 'should re-queue unless last question was shown more than 10 seconds ago' do
+  it 'should perform requeue unless last question was shown more than 10 seconds ago' do
     channel.last_question_at = Time.now
     channel.save
+    job.expects(:requeue).with(channel, 'user', 'Question')
+    job.perform(channel.id, 'user', 'Question')
+  end
+
+  it 'should perform clear_layer when last question was shown more than 10 seconds ago' do
+    channel.last_question_at = 12.seconds.ago
+    channel.save
+    job.expects(:clear_layer).with(channel)
+    job.perform(channel.id)
+  end
+
+  it 'should not perform clear_layer unless last question was shown more than 10 seconds ago' do
+    channel.last_question_at = Time.now
+    channel.save
+    job.expects(:clear_layer).never
+    job.perform(channel.id)
+  end
+
+  it 'should requeue' do
     QuestionsDisplayJob.expects(:perform_later).with(
       channel.id,
       'user',
       'Question'
     )
-    job.perform(channel.id, 'user', 'Question')
+    job.send(:requeue, channel, 'user', 'Question')
   end
 
   it 'should set_layer' do
@@ -44,6 +63,23 @@ RSpec.describe QuestionsDisplayJob, type: :active_job do
     Eyeson::Layer.expects(:new).never
     channel.expects(:update).never
     job.send(:set_layer, channel, 'user', 'Question')
+  end
+
+  it 'should clear_layer' do
+    access_key = Faker::Crypto.md5
+    channel.access_key = access_key
+    channel.save
+    Eyeson::Layer.expects(:new).with(access_key).returns(mock('Layer', destroy: true))
+    channel.expects(:update)
+    job.send(:clear_layer, channel)
+  end
+
+  it 'should not clear_layer without access_key' do
+    channel.access_key = nil
+    channel.save
+    Eyeson::Layer.expects(:new).never
+    channel.expects(:update).never
+    job.send(:clear_layer, channel)
   end
 
   it 'should create a question image' do
